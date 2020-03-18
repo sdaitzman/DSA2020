07a273280266cf54b47fe9127538ba6b317d3ed2
Sam Daitzman <sam@daitzman.com>
2020-03-17 22:23:53 -0400

    uploads for debugging help

--- Week 4/sdaitzman.py
+++ Week 4/sdaitzman.py
@@ -46,14 +46,13 @@ def choose_meal(happiness_scores, start_index=0, end_index=None):
     end_index:     end index in overall list
     """
 
-    # set end index if not set
-    end_index = end_index if end_index else len(happiness_scores)-1
-
     print(start_index, end_index)
 
+    # set end index if not set
+    if end_index == None: end_index = len(happiness_scores)-1
+
     # return list at base case
-    if start_index == end_index:
-        return (start_index, end_index, happiness_scores[start_index])
+    if start_index == end_index: return (start_index, end_index, happiness_scores[start_index])
 
     # find the middle of the list
     middle_index = int((start_index + end_index) / 2)
@@ -66,15 +65,24 @@ def choose_meal(happiness_scores, start_index=0, end_index=None):
     (right_start_index, right_end_index, right_sum) = choose_meal(happiness_scores, middle_index+1, end_index)
 
     # return greatest value
-    if  left_sum  >= right_sum and left_sum >= middle_sum: return (left_start_index , left_end_index , left_sum ) 
-    if  right_sum >= left_sum and right_sum >= middle_sum: return (right_start_index, right_end_index, right_sum)
-    if middle_sum >= left_sum and middle_sum >= right_sum: return (middle_start_index, middle_end_index, middle_sum)
-
-    return None
-
-
+    if  left_sum  >= right_sum and left_sum >= middle_sum:
+        print('left')
+        return (left_start_index , left_end_index , left_sum ) 
+    if  right_sum >= left_sum and right_sum >= middle_sum:
+        print('right')
+        return (right_start_index, right_end_index, right_sum)
+    if middle_sum >= left_sum and middle_sum >= right_sum:
+        print('middle')
+        return (middle_start_index, middle_end_index, middle_sum)
 
 options = ["french fries", "brussel sprouts", "chicken sandwiches", "tomato soup", "fruit salad"]
+
 scores = [1, 3, 4, -100, 25]
-choose_meal(scores)
-choose_meal(scores, 0, 10)
+# print(choose_meal(scores))
+
+scores = [1, 3, 4, 25]
+print(choose_meal(scores))
+
+
+scores = [25, 1, -8, 3, 4]
+# print(choose_meal(scores))
