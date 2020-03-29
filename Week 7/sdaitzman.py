8134e097e9b306d36afc5a4420f2383ef0ae48b9
Sam Daitzman <sam@daitzman.com>
2020-03-29 19:36:56 -0400

    finishes exercise 2

--- Week 7/sdaitzman.py
+++ Week 7/sdaitzman.py
@@ -16,27 +16,48 @@ def count_components(graph):
     nx.set_node_attributes(graph, False, 'visited')
 
     # iterate through all nodes
-    for i,e in enumerate(graph.nodes(data=True)):
-        # i: index we're currently at
-        # e: node we are currently at (tuple of label and attributes dict)
-
-        # if we haven't seen this component yet
+    for e in graph.nodes(data=True):
         node = graph.nodes[e[0]]
-        node["foo"] = "bar"
-        print(graph.nodes(data=True))
 
-        if not e[1]['visited']:
-            # print("Another one: ", e)
+        # if we haven't seen this component yet, mark it as visited and descend
+        if not node['visited']:
+            node['visited'] = True
             components += 1
-        
+            remaining = [e[0]] # stack for DFS
+            while remaining:
+                current_node = remaining.pop()
+                for adjacent in graph.neighbors(current_node):
+                    if not graph.nodes[adjacent]['visited']:
+                        graph.nodes[adjacent]['visited'] = True
+                        remaining.append(adjacent)
+    return components
+
+def test_count_components():
+    ''' Tests the count_components function '''
+    G = nx.Graph()
+
+    G.add_node(1)
+    G.add_node(2)
+    G.add_node(3)
+    G.add_node(4)
+    G.add_node(5)
+    G.add_node(6)
+
+    # component 1:
+    G.add_edge(1,2)
+
+    # component 2:
+    G.add_edge(3,4)
+    G.add_edge(4,5)
+    G.add_edge(5,3)
+
+    # component 3:
+    # node 6 is alone :'(
+
+    # check the count is correct
+    assert count_components(G) == 3
+
+    # check that we visited all nodes
+    for e in G.nodes(data=True): assert e[1]['visited'] == True
 
 
-G = nx.Graph()
-G.add_node(1) # adds node with label 1
-G.add_node(2) # adds node with label 2
-G.add_node(200) # adds node with label 2
-G.add_edge(1,2) # adds an undirected edge between nodes 1 and 2
-G.edges() # returns an iterator over all edges
-G.nodes() # returns an iterator over all nodes
-G.neighbors(1) # returns the neighbors of node 1
-count_components(G)
