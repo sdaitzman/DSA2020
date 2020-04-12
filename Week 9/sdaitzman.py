0891c3c33d0b1557867f93fd88112a0846d53c16
Sam Daitzman <sam@daitzman.com>
2020-04-11 22:36:04 -0400

    implements testing

--- Week 9/sdaitzman.py
+++ Week 9/sdaitzman.py
@@ -13,8 +13,9 @@ def wildcard_string_compare(s1, s2):
     i = 0;
     j = 0;
 
-    starIndex = -1;
-    iIndex = -1;
+    # initialize pointers within text
+    wildcard = -1;
+    text_index = -1;
 
 
     # while not at end of text
@@ -28,17 +29,17 @@ def wildcard_string_compare(s1, s2):
         # if not at end and hit wildcard, set pointer to current wildcard and
         # set location in text to current index and iterate through wildcard
         elif(j < n and s2[j] == '*'):
-            starIndex = j
-            iIndex = i
+            wildcard = j
+            text_index = i
             j = j + 1
 
         # if none of these checks have happened and we have some wildcard so far
         # iterate position past wildcard in both strings and continue to next
         # letter in text string we're checking
-        elif(starIndex != -1):
-            j = starIndex + 1
-            i = iIndex + 1
-            iIndex += 1
+        elif(wildcard != -1):
+            j = wildcard + 1
+            i = text_index + 1
+            text_index += 1
 
         # if none of these cases are true, under the principle of optimality
         # we must return False, since the strings cannot match
@@ -51,19 +52,44 @@ def wildcard_string_compare(s1, s2):
     return j == n
 
 
-s1 = 'hi'
-s2 = 'h*'
-print(wildcard_string_compare(s1, s2))
+def test_wildcards():
+    s1 = 'hi'
+    s2 = 'h*'
+    assert wildcard_string_compare(s1, s2)
 
+    s1 = 'hello'
+    s2 = 'h*llo'
+    assert wildcard_string_compare(s1, s2)
 
-s1 = 'hello'
-s2 = 'h*llo'
-print(wildcard_string_compare(s1, s2))
+    s1 = 'heeeeello'
+    s2 = 'h*llo'
+    assert wildcard_string_compare(s1, s2)
 
-s1 = 'heeeeello'
-s2 = 'h*llo'
-print(wildcard_string_compare(s1, s2))
+    s1 = 'foo'
+    s2 = 'h*llo'
+    assert not wildcard_string_compare(s1, s2)
 
-s1 = 'foo'
-s2 = 'h*llo'
-print(wildcard_string_compare(s1, s2))
+    s1 = 'lemondrop'
+    s2 = 'l*dr*p*'
+    assert wildcard_string_compare(s1, s2)
+    
+    s1 = 'lemondrop'
+    s2 = 'lem*m*dr*p'
+    assert not wildcard_string_compare(s1, s2)
+
+    s1 = ''
+    s2 = ''
+    assert wildcard_string_compare(s1, s2)
+
+    s1 = ''
+    s2 = '*'
+    assert wildcard_string_compare(s1, s2)
+
+    s1 = 'foobarbaz'
+    s2 = 'foo*'
+    assert wildcard_string_compare(s1, s2)
+
+    s1 = 'foobarbaz'
+    s2 = 'foo*z'
+    assert wildcard_string_compare(s1, s2)
+    
